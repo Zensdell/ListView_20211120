82d3f5971d5293c5f0e6bc5c03ef10e3b09bd8d0
구영모 <dudah754@naver.com>
2021-11-20 15:45:20 +0900

    리스트뷰의 롱클릭 이벤트 처리   - true를 리턴해서, 롱클릭만 따로 이벤트 처리

--- app/src/main/java/com/nepplus/listview_20211120/MainActivity.kt
+++ app/src/main/java/com/nepplus/listview_20211120/MainActivity.kt
@@ -39,6 +39,16 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "${clickedStudent.name} 클릭됨", Toast.LENGTH_SHORT).show()
             
         }
+        
+        studentListView.setOnItemLongClickListener { parent, view, position, id -> 
+            
+//            길게 눌렸을 때의 별도 행동.
+            val longClickedStudent = mStudentList[position]
+
+            Toast.makeText(this, "${longClickedStudent.name} 길게 클릭됨", Toast.LENGTH_SHORT).show()
+            return@setOnItemLongClickListener true
+            
+        }
 
 
     }
