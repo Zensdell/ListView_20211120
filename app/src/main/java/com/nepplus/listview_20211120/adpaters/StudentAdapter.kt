049c5f77f0a96d9295bd9b32701e1e6ec33d150a
구영모 <dudah754@naver.com>
2021-11-20 12:48:43 +0900

    Adapter 클래스 기초 작업 마무리   - xml -> 어댑터에서 row 변수에 담아주자.

--- app/src/main/java/com/nepplus/listview_20211120/adpaters/StudentAdapter.kt
+++ app/src/main/java/com/nepplus/listview_20211120/adpaters/StudentAdapter.kt
@@ -1,11 +1,39 @@
 package com.nepplus.listview_20211120.adpaters
 
 import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.nepplus.listview_20211120.R
 import com.nepplus.listview_20211120.datas.StudentData
 //스튜던트 어댑터의 하위 데이터를 받아옵니다, ex)StudentData  어디서 mContext어떤 모양으로?resId 어떤 목록을mList?
 class StudentAdapter(
     val mContext: Context,
     val resId:Int,
     val mList : ArrayList<StudentData>) : ArrayAdapter<StudentData>(mContext,resId,mList){
+
+//        Adapter 역할: xml + 데이터 클래스 자료 조합.
+
+//        xml파일을 - >이 어댑터 코드에서 끌어다 사용하게 도와줌
+        val mInflater = LayoutInflater.from(mContext)
+
+//        xml + 데이터 조합 -> 위치에 ㅂ뿌려주는 함수 (ArrayAdapter 제공) -> 커스터마이징 (함수 오버라이딩)
+//        position 위치 0,1,2,.... convertView 메모리 줄이는 용도
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        
+        var tempRow = convertView
+    
+        if(tempRow == null){
+            tempRow = mInflater.inflate(R.layout.student_list_item,null)
+        }
+        val row = tempRow!!
+        return row
+
+    }
+
+
+
+
+
 }
